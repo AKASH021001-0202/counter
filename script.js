70c30a8bc9fb4f9b0d487851e34530f49647a644
AKASH021001-0202 <nakashn007@gmail.com>
2024-02-23 22:58:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+// script.js
+
+// Callback hell to deploy the timer using arrow functions
+setTimeout(() => {
+    updateCountdown(10);
+    setTimeout(() => {
+        updateCountdown(9);
+        setTimeout(() => {
+            updateCountdown(8);
+            setTimeout(() => {
+                updateCountdown(7);
+                setTimeout(() => {
+                    updateCountdown(6);
+                    setTimeout(() => {
+                        updateCountdown(5);
+                        setTimeout(() => {
+                            updateCountdown(4);
+                            setTimeout(() => {
+                                updateCountdown(3);
+                                setTimeout(() => {
+                                    updateCountdown(2);
+                                    setTimeout(() => {
+                                        updateCountdown(1);
+                                        document.querySelector('.countdown').textContent = "Happy Independence!";
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
+
+function updateCountdown(count) {
+    document.querySelector('.countdown').textContent = count;
+}
